d5f2b2a71e76c8ae7920552f415f53687e9447b3
PetrIvChe <cherniavskiipetr@gmail.com>
2022-01-02 12:02:24 +0300

    Пополнение на 100 рублей, 0бонусных.

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int account = 100;
+        int replenishmentAmount = 100;
+        int bonusRuble;
+        int accountWithReplenish;
+        if ( replenishmentAmount >=1000){
+            bonusRuble = replenishmentAmount/100;
+        }else {
+            bonusRuble = 0;
+        }
+        accountWithReplenish = account +  replenishmentAmount + bonusRuble;
+        System.out.println("На вашем счёте " + accountWithReplenish + " из них начисленно " + bonusRuble + " бонусных рублей");
+    }
+}
