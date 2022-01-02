4900b399a6b82d490273f1d33244bcd37ffc37cd
PetrIvChe <cherniavskiipetr@gmail.com>
2022-01-02 12:06:25 +0300

    Пополнение на 1100 и бонусных 11 рублей

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int account = 100;
-        int replenishmentAmount = 1001;
+        int replenishmentAmount = 1100;
         int bonusRuble;
         int accountWithReplenish;
         if ( replenishmentAmount >=1000){
